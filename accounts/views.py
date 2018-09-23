605915c7699cfc74e94982ade68257dd67c63977
Sarah Loh <sarah.loh0708@gmail.com>
2018-09-23 16:13:38 +0100

    Add login form and template and handle GET request in views/urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from accounts.forms import UserLoginForm
 
 # Create your views here.
 def index(request):
@@ -15,3 +16,10 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You have successfully been logged out.")
     return redirect(reverse('index'))
+
+def login(request):
+    """
+    Return a login page
+    """
+    login_form = UserLoginForm()
+    return render(request, "login.html", {"login_form": login_form})
--- django_auth/urls.py
+++ django_auth/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('accounts/logout/', views.logout, name="logout"),
+    path('accounts/login/', views.login, name="login"),
 ]
